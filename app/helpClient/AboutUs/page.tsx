6e024590595d0402c47282476ab5b7fe4ff741af
Hienhayhat <hienythb123@gmail.com>
2024-10-30 09:30:05 +0700

    update

--- app/helpClient/AboutUs/page.tsx
+++ app/helpClient/AboutUs/page.tsx
@@ -1,7 +1,13 @@
 const AboutUsPage = () => {
     return (
         <div>
-            <p>chúng tôi là những người chơi code</p>
+            <h2>chúng tôi là Hiển và Dũng cùng nhau tạo lên trang web này</h2>
+            <h3>Hiển tên đầy đủ là Lương Hoàng Hiển
+                sinh ngày:14/04/2005
+                sinh viên ngành CNTT ,Học viện kĩ thuật mật mã.
+            </h3>
+            <p>Hiển là một người rất thích trải nghiệm sáng tạo nhiều thứ </p>
+            <p>Dũng là </p>
         </div>
     )
 
--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -5,16 +5,18 @@ import Link from "next/link"
 
 const FooterApp = (): any => {
     return (
-        <div >
-            <Footer className="relative ">
-                <Link href="/helpclient/FAQ"  ><div className="text-1xl absolute right-2 top-5 ">FAQ</div></Link>
-                <Link href="/helpclient/aboutUs"><div className="text-1xl absolute right-2 top-[50%] ">About Us</div></Link>
-                <Link href="https://www.facebook.com/profile.php?id=100076069864342" className="text-2xl"><FacebookFilled />Hiển</Link>
-                <Link href="https://www.facebook.com/profile.php?id=100080934456147" className="text-2xl"><FacebookFilled /> Dũng</Link>
-                <Link href="https://github.com/Hienhayhat/ProjectBig" className="text-2xl"><GithubFilled />github</Link>
+        <div className="fixed bottom-0 w-[100%] " >
+            <Footer className="relative h-[100px]">
+                <Link href="/helpclient/FAQ"  ><div className="text-1xl sm:text-2xl absolute right-2 top-5 ">FAQ</div></Link>
+                <Link href="/helpclient/aboutUs"><div className="text-1xl sm:text-2xl absolute right-2 top-[50%] ">About Us</div></Link>
+                <div className="flex flex-col absolute left-1 top-1">
+                    <Link href="https://www.facebook.com/profile.php?id=100076069864342" className="text-1xl sm:text-2xl "><FacebookFilled />Hiển</Link>
+                    <Link href="https://www.facebook.com/profile.php?id=100080934456147" className="text-1xl sm:text-2xl "><FacebookFilled /> Dũng</Link>
+                    <Link href="https://github.com/Hienhayhat/ProjectBig" className="text-1xl sm:text-2xl "><GithubFilled />github</Link>
+                </div>
 
 
-                <div className="absolute right-[47%] bottom-[47%] text-2xl ">created by @hiendung</div>
+                <div className=" flex justify-center align-middle tex ">created by @hiendung</div>
             </Footer>
         </div>
 
