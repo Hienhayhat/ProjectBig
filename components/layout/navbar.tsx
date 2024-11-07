ec348c7e3596e5554d687c60ae3a793a720ee78a
unknown <dunggawr>
2024-11-07 22:37:52 +0700

    update

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -12,33 +12,35 @@ import {
 } from "@/components/ui/navigation-menu";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const locale = ["Vi", "En"];
 
 export const NavBar = () => {
   const [input, setInput] = useState<string>("");
+  const router = useRouter()
   return (
-    <div className="sticky top-0 bg-white px-[16px] md:px-[80px] z-10 custom-bg pt-2 pb-3">
+    <header className="sticky top-0 bg-white px-[16px] md:px-[80px] z-10 custom-bg pb-3">
       <div className="w-full flex justify-between m-1">
         <div></div>
         <div className="flex gap-6">
           <Link
             href="/"
-            className="flex items-center transition duration-300 hover:bg-accent px-2 rounded-md"
+            className="flex items-center transition duration-300 hover:text-gray-500 px-2 rounded-md"
           >
             Hỗ trợ
           </Link>
           <NavigationMenu>
             <NavigationMenuList>
               <NavigationMenuItem>
-                <NavigationMenuTrigger className="">VI</NavigationMenuTrigger>
+                <NavigationMenuTrigger className="hover:text-gray-500">VI</NavigationMenuTrigger>
                 <NavigationMenuContent>
                   <div className="flex flex-col w-[61.23px] text-center">
                     {locale?.map((item) => (
                       <div
                         key={item}
-                        className="m-1 transition hover:bg-accent rounded-sm cursor-pointer"
+                        className="m-1 transition hover:text-gray-500 rounded-sm cursor-pointer"
                       >
                         {item}
                       </div>
@@ -50,13 +52,13 @@ export const NavBar = () => {
           </NavigationMenu>
           <Link
             href="/register"
-            className="flex items-center transition duration-300 hover:bg-accent px-2 rounded-md"
+            className="flex items-center transition duration-300 hover:text-gray-500 px-2 rounded-md"
           >
             Đăng ký
           </Link>
           <Link
             href="/signin"
-            className="flex items-center transition duration-300 hover:bg-accent px-2 rounded-md"
+            className="flex items-center transition duration-300 hover:text-gray-500 px-2 rounded-md"
           >
             Đăng nhập
           </Link>
@@ -64,7 +66,9 @@ export const NavBar = () => {
       </div>
 
       <div className="relative flex items-center px-10 h-full justify-between">
-        <div className="flex flex-row items-center gap-1">
+        <div className="flex flex-row items-center gap-1 cursor-pointer" onClick={() => {
+          router.push('/')
+        }}>
           <div className="relative w-10 h-10 overflow-hidden">
             <Image
               src="/images/cover.png"
@@ -100,6 +104,6 @@ export const NavBar = () => {
           <ShoppingCartIcon width={40} height={40} />
         </div>
       </div>
-    </div>
+    </header>
   );
 };
